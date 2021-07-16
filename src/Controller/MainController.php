4f3abd4b9feac27b5234c28a701fbf584e50473e
Hayden Schiff <haydenschiff@gmail.com>
2021-07-16 16:56:31 -0400

    update about page

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -29,7 +29,7 @@ class MainController extends AbstractController
     }
 
     /**
-     * @Route("/about", name="main_contact")
+     * @Route("/about", name="main_about")
      */
     public function about(): Response
     {
