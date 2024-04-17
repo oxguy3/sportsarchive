dc8d8b29613490f92c0ec4c80c4e921809494d60
Hayden Schiff <hayden@schiff.io>
2024-04-16 22:35:56 -0400

    fix Doctrine deprecation issue

--- src/Kernel.php
+++ src/Kernel.php
@@ -2,7 +2,11 @@
 
 namespace App;
 
+use Doctrine\DBAL\Platforms\PostgreSQLPlatform;
+use Doctrine\ORM\Mapping\ClassMetadata;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
 class Kernel extends BaseKernel
@@ -13,5 +17,28 @@ class Kernel extends BaseKernel
     {
         date_default_timezone_set('UTC');
         parent::boot();
+    }
+
+    /**
+     * This prevents a deprecation notice for Doctrine ID generation from
+     * appearing, per https://github.com/doctrine/orm/issues/8893#issuecomment-1905791768
+     */
+    #[\Override]
+    protected function build(ContainerBuilder $container): void
+    {
+        $container->addCompilerPass(new class() implements CompilerPassInterface {
+            public function process(ContainerBuilder $container): void
+            {
+                $container->getDefinition('doctrine.orm.default_configuration')
+                    ->addMethodCall(
+                        'setIdentityGenerationPreferences',
+                        [
+                            [
+                                PostgreSQLPlatform::class => ClassMetadata::GENERATOR_TYPE_SEQUENCE,
+                            ],
+                        ]
+                    );
+            }
+        });
     }
 }
